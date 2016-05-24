6ca7cfae90ebc2ae7fe8a8c8f7a4694b9a39e733
Lukas Kluft <lukas.kluft@gmail.com>
2016-05-24 08:00:47 +0200

    clb is now a full python project.

---
+++ clb/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='clb',
+    author='Lukas Kluft',
+    author_email='lukas.kluft@gmail.com',
+    url='https://github.com/lkluft/cloud-base',
+    version='0.1',
+    packages=find_packages(),
+    license='LICENSE.txt',
+    include_package_data=True,
+    install_requires=[
+        'matplotlib>=1.5.1',
+        'numpy>=1.10.4',
+        'scipy>=0.17.1',
+        'typhon>=0.3.0',
+    ],
+)
